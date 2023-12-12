2395c67094693a314547b9c8cb058e8aed92e8c2
dquoc1602 <143944928+dquoc1602@users.noreply.github.com>
2023-12-12 16:24:17 +0700

    Update

--- src/main/java/com/example/capstone/repository/Cart/ICartDetailRepository.java
+++ src/main/java/com/example/capstone/repository/Cart/ICartDetailRepository.java
@@ -4,6 +4,7 @@ import com.example.capstone.model.Cart.Cart;
 import com.example.capstone.model.Cart.CartDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,8 +12,12 @@ import java.util.List;
 @Repository
 public interface ICartDetailRepository extends JpaRepository<CartDetail, Integer> {
 
-    @Query("")
+    //@Query("")
     List<CartDetail> findByCart(Cart cart);
 
     void deleteByCart(Cart cart);
+
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM cart_detail WHERE cart_id = :id")
+    List<CartDetail> findByCartId(@Param("id") Integer id);
 }
--- src/main/java/com/example/capstone/repository/Cart/ICartRepository.java
+++ src/main/java/com/example/capstone/repository/Cart/ICartRepository.java
@@ -3,6 +3,7 @@ package com.example.capstone.repository.Cart;
 import com.example.capstone.model.Cart.Cart;
 import com.example.capstone.model.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
